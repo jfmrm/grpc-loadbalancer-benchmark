e5f65c15c02a9db57a0b288a4d11959224ee245f
Joao Filipe Moura <joao.moura@flugel.it>
2022-04-10 23:13:18 -0300

    add basic prometheus instrumentation

--- server/main.go
+++ server/main.go
@@ -6,8 +6,13 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	pb "github.com/jfmrm/grpc-loadbalancer-benchmark/helloworld"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
 )
 
@@ -20,11 +25,16 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 }
 
 var (
-	port = flag.Int("port", 50051, "The server port")
+	port        = flag.Int("port", 50051, "The server port")
+	metricsPort = flag.Int("metrics-port", 2112, "The metrics port")
 )
 
 func main() {
 	flag.Parse()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
 	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -32,9 +42,22 @@ func main() {
 	s := grpc.NewServer()
 
 	pb.RegisterHelloServiceServer(s, &server{})
-	log.Printf("Starting server on port %d", *port)
 
-	if err := s.Serve(listen); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	go func() {
+		log.Printf("Starting server on port %d", *port)
+		if err := s.Serve(listen); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	go func() {
+		log.Printf("Starting metrics server on port %d", *metricsPort)
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":2112", nil)
+	}()
+
+	<-done
+
+	log.Println("Shutting down server...")
+	s.GracefulStop()
 }
